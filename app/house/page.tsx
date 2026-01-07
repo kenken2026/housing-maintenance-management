57d94ddb48b71677a133a726af7b6031aa53fb78
kixixixixi <kixixixixi@gmail.com>
2026-01-07 19:48:59 +0900

    Add dialog

--- app/house/page.tsx
+++ app/house/page.tsx
@@ -1,10 +1,11 @@
 "use client"
-
+import { ask } from "@tauri-apps/plugin-dialog"
 import { Card } from "components/elements"
 import { useSearchParams, notFound } from "next/navigation"
 import { useTeamState } from "lib/store"
 import { FC, useEffect, useState } from "react"
 import { houseModel } from "lib/models"
+import { Button } from "components/elements/form"
 
 const Page: FC = () => {
   const { team } = useTeamState()
@@ -27,13 +28,45 @@ const Page: FC = () => {
       {house && (
         <Card>
           <h2>{house.name}</h2>
-          <div>
-            <dl>
-              <dt>緯度</dt>
-              <dd>{house.latitude}</dd>
-              <dt>経度</dt>
-              <dd>{house.longitude}</dd>
-            </dl>
+          <p style={{ fontSize: ".75rem" }}>UID: {house.uid}</p>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <table>
+              <tbody>
+                <tr>
+                  <td>緯度</td>
+                  <td style={{ textAlign: "right" }}>{house.latitude}</td>
+                </tr>
+                <tr>
+                  <td>経度</td>
+                  <td style={{ textAlign: "right" }}>{house.longitude}</td>
+                </tr>
+                <tr>
+                  <td>標高</td>
+                  <td style={{ textAlign: "right" }}>{house.altitude}</td>
+                </tr>
+                <tr>
+                  <td>階数</td>
+                  <td style={{ textAlign: "right" }}>{house.floorCount}</td>
+                </tr>
+                <tr>
+                  <td>部屋数</td>
+                  <td style={{ textAlign: "right" }}>{house.roomCount}</td>
+                </tr>
+                <tr>
+                  <td>階段数</td>
+                  <td style={{ textAlign: "right" }}>{house.stepCount}</td>
+                </tr>
+              </tbody>
+            </table>
+            <div>
+              <Button
+                onClick={() =>
+                  ask(`「${house.name}」を削除してもよろしいでしょうか`, "確認")
+                }
+              >
+                削除
+              </Button>
+            </div>
           </div>
         </Card>
       )}
