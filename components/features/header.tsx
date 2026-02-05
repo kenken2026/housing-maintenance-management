bb169572c63e3059fda308e3232878c2f3ee3577
kixixixixi <kixixixixi@gmail.com>
2026-02-05 19:46:46 +0900

    Add home icon

--- components/features/header.tsx
+++ components/features/header.tsx
@@ -19,11 +19,28 @@ export const Header: FC = () => {
       <Link
         href="/"
         style={{
+          alignItems: "center",
+          display: "flex",
           fontSize: "1rem",
           fontWeight: "bold",
+          gap: ".25rem",
           margin: 0,
         }}
       >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+          <polyline points="9 22 9 12 15 12 15 22" />
+        </svg>
         公共賃貸住宅維持管理
       </Link>
       <p
